97f3bf7c40233eb41087a9d13129383078e4a215
dbfjru <dbfjru@gmail.com>
2024-01-24 10:00:59 +0900

    Day17 (24.01.24) level3 #47 complete

--- src/level3/Prob47.java
+++ src/level3/Prob47.java
@@ -1,47 +1,20 @@
 package level3;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Prob47 {
     //Title : 문자열 내 마음대로 정렬하기
     static class Solution {
         public String[] solution(String[] strings, int n) {
-            String[] answer = {};
-            String temp = "";
-            for(int i = 0; i<strings.length; i++){
-                for(int j= i+1; j<strings.length; j++ ){
-                    if(strings[i].charAt(n) > strings[j].charAt(n)){
-                        System.out.println("big case");
-                        temp = strings[i];
-                        strings[i] = strings[j];
-                        strings[j] = temp;
-                    }else if(strings[i].charAt(n) == strings[j].charAt(n)){
-                        System.out.println("equal case");
-                        char[] arrI = strings[i].toCharArray();
-                        char[] arrJ = strings[j].toCharArray();
-                        int length = Math.min(arrI.length,arrJ.length);
-                        for(int k = 0; k<length ; k++){
-                            if(arrI[k] > arrJ[k]){
-                                temp = strings[i];
-                                strings[i] = strings[j];
-                                strings[j] = temp;
-                                break;
-                            }else if(arrI[k] < arrJ[k]){
-                                break;
-                            }else{
-                                System.out.println("same character at "+ k);
-                            }
-                        }
-                    }else{
-                        System.out.println("small case");
-                    }
-                }
+            String[] answer = new String[strings.length];
+            ArrayList<String> arr = new ArrayList<>();
+            for(String str : strings){ // 기준이 되는 문자를 맨 앞에 추가하여 정렬 간편히
+                arr.add(str.charAt(n)+str);
             }
-            answer = strings;
-            for(String str : answer){
-                System.out.println(str);
+            Collections.sort(arr); // 정렬
+            for(int i =0; i < arr.size(); i++){
+                answer[i] = arr.get(i).substring(1);//맨 앞 글자 제외하고 나머지 할당
             }
             return answer;
         }
@@ -49,7 +22,8 @@ public class Prob47 {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String[] input = {"abce", "abc", "cdx"};
+        String[] input = {"sun", "bed", "chair", "car","sports","bedboy","sporex"};
+
         sol.solution(input,2);
     }
 }
@@ -57,4 +31,5 @@ public class Prob47 {
 //        strings   	    n	        return
 //["sun" , "bed",  "car"]	1	["car" , "bed" , "sun"]
 //["abce", "abcd", "cdx"]	2	["abcd", "abce", "cdx"]
-//미완성..
+//결국 다른 사람의 풀이 확인 했음
+//기준이 되는 문자를 맨 앞에 추가한 뒤 정렬하는 방법은 아주 기발한듯
